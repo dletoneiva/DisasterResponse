3d50ecb49aa42a733d658160f0d38708997f4302
Daniel Leto <daniel.leto@dtidigital.com.br>
2021-11-18 18:11:15 -0300

    added docstrings to train_classifier

--- workspace/models/train_classifier.py
+++ workspace/models/train_classifier.py
@@ -14,20 +14,29 @@ from sqlalchemy import create_engine
 
 
 # model libraries
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.naive_bayes import GaussianNB, BernoulliNB
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.multioutput import MultiOutputClassifier
 
 # model storage
 import pickle 
 
-# class used to transform data to an acceptable format for GaussianNB
+# 
 class DenseTransformer(BaseEstimator, TransformerMixin):
+    """
+    Class used to transform data to an acceptable format for GaussianNB
+
+    Args:
+        BaseEstimator ([object]): sklearn estimator
+        TransformerMixin ([object]): sklearn transformer
+    
+    Returns:
+        Transformed model
+    """    
 
     def fit(self, X, y=None, **fit_params):
         return self
@@ -37,9 +46,20 @@ class DenseTransformer(BaseEstimator, TransformerMixin):
 
 
 def load_data(database_filepath):
+    """
+    Loads data to be used in the model
+
+    Args:
+        database_filepath ([path]): Path for the SQL database.
+
+    Returns:
+        [type]: [description]
+    """    
     # load data from database
     engine = create_engine('sqlite:///categorized_messages.db')
-    df = pd.read_sql('select * from Cat',engine)[:100]
+    df = pd.read_sql('select * from Cat',engine) # [:100] # uncomment to run faster
+
+
     
     # define columns to be used
     Y_cols = ['related', 'request', 'offer',
@@ -60,6 +80,15 @@ def load_data(database_filepath):
     return X, Y, Y_cols
 
 def tokenize(text):
+    """
+    Tokenizes text data, lemmatizes it and removes URL's.
+
+    Args:
+        text ([string]): String containing the text to be tokenized.
+
+    Returns:
+        [list of strings]: List containing tokenized text
+    """    
     # filter URL's using regex
     url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     detected_urls = re.findall(url_regex, text)
@@ -80,6 +109,12 @@ def tokenize(text):
 
 
 def build_model():
+    """
+    Instantiates the model to be trained.
+
+    Returns:
+        [object]: sklearn object.
+    """    
     # build it as a pipeline
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
@@ -98,10 +133,38 @@ def build_model():
     return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """
+    Evaluates the model, printing all the scores using classification_report.
+
+    Args:
+        model ([object]): Model to be evaluated.
+        X_test ([object]): Pandas dataframe object containing the text (1D).
+        Y_test ([object]): Pandas dataframe object containing the classes.
+        category_names ([list of strings]): Category names to be evaluated.
+    """    
+    # predict based on X_test
+    y_pred = model.predict(X_test)
+
+    # transform to dataframe to be easier to read
+    df_y_pred = pd.DataFrame(y_pred)
+    df_y_pred.columns = category_names
+
+    for col in category_names:
+        print('Analysed class: '+ str(col) + '\n')
+        print('Bernoulli Naive Bayes (with GridSearch) classification: \n')
+        print(classification_report(Y_test[col], 
+                                    df_y_pred[col]))
+
 
 
 def save_model(model, model_filepath):
+    """
+    Saves the model in selected path.
+
+    Args:
+        model ([object]): Trained model to be saved.
+        model_filepath ([path]): Filepath for pickle file.
+    """    
     pickle.dump(model, open(model_filepath,'wb'))
 
 
